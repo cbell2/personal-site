70f92c00d294bdd4f5770c4a4b05904e7de049ac
cbell2 <Devlingithub!2018>
2024-08-25 13:03:50 -0400

    Added react-router-dom, added routing w/ Home, Projects, Contact, and 404 Page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <ul>
+        <li>
+          <a href="/projects">Projects</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Home;
